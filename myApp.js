f2ce0213371e03e0222fa933f05f9aef509a41da
Rethabile Liphalane <rethabileliphalane213@gmail.com>
2026-04-13 12:15:45 +0200

    fix:add request

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 
-app.get("/expess", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hello World");
-});
+}); */
 
 app.get("/",(req,res)=>{
     res.send("Hello Express")
